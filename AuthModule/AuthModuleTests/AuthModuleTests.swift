84e35bd6f1c2c27f036dec7eb2fb2cb048bf15f0
Pawan selokar <pawanselokar@Pawans-MacBook-Air.local>
2025-01-16 09:55:55 +0530

    Resource folder is created

---
+++ AuthModule/AuthModuleTests/AuthModuleTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuthModuleTests.swift
+//  AuthModuleTests
+//
+//  Created by Pawan selokar on 16/01/25.
+//
+
+import Testing
+@testable import AuthModule
+
+struct AuthModuleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
